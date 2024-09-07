852e87f01b0bcd06631bfb1e0c25b88127c91e31
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-07 13:10:53 +0500

    Update Q4.c

--- Lab 3/Q4.c
+++ Lab 3/Q4.c
@@ -1,1 +1,26 @@
+/*Q4: A car travelled back and forth from point A to point B. With a distance being (single trip) 1207KM. During
+the forward trip fuel price was 118/liter while returning it was 123/liter. Calculate the total fuel cost
+(both ways) and the fuel consumed (total trip). Use the car’s fuel average as input from the user (Input
+must be positive make some restrictions on only accepting positive input)*/
 
+#include <stdio.h>
+int main()
+{
+  int Trip = 1207;
+  float Favg, Liter, FcostA, FcostB, Tcost;
+  printf("Enter Fuel Average: ");
+  scanf("%f", &Favg);
+  if (Favg < 0)
+  {
+  	printf("Fuel Average is out of bound");
+  	return 0;
+  }
+  Liter = Trip/Favg;
+  FcostA = 118 * Liter;
+  FcostB = 123 * Liter;
+  Tcost = FcostA + FcostB;
+  printf("The total Fuel cost of the trip is $%.2f\n",Tcost);
+  Liter += 1;
+  printf("The total fuel consumed is %.2f\n", Liter);
+  return 0;
+} 
