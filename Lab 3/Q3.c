21af2f56f17e36a28e1e3693051f50c79c24cbd5
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-07 12:41:48 +0500

    Update Q3.c

--- Lab 3/Q3.c
+++ Lab 3/Q3.c
@@ -1,6 +1,6 @@
-Q3: A customer asks the IT firm to develop a program in C language, which can take tax rate and salary from
+/*Q3: A customer asks the IT firm to develop a program in C language, which can take tax rate and salary from
 the user on runtime and then calculate the tax, the user has to pay and the salary he/she will have after
-paying the tax. This information is then provided to the user.
+paying the tax. This information is then provided to the user.*/
 
 #include <stdio.h>
 int main()
