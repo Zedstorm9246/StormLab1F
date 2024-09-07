955087fe75d26a8191287d32b8755cf1f3bd5426
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-07 12:35:06 +0500

    Update Q3.c

--- Lab 3/Q3.c
+++ Lab 3/Q3.c
@@ -1,5 +1,6 @@
-Q3:
-
+Q3: A customer asks the IT firm to develop a program in C language, which can take tax rate and salary from
+the user on runtime and then calculate the tax, the user has to pay and the salary he/she will have after
+paying the tax. This information is then provided to the user.
 
 #include <stdio.h>
 int main()
@@ -7,13 +8,13 @@ int main()
 	float TaxR;
 	float Salary;
 	printf("Enter your Salary: ");
-	scanf("%f", &TaxR );
+	scanf("%f", &Salary );
 	printf("Enter Tax rate: ");
-	scanf("%d", &Salary);
+	scanf("%f", &TaxR);
 	TaxR = TaxR/100;
-	Salary = Salary - (Salary * TaxR);
+	Salary = Salary *(1-TaxR);
 	printf("Your Salary after tax deduction is: %0.2f", Salary);
-} 	
+} 
 
 
-wrong
+
