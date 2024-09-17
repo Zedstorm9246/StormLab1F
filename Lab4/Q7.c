95fd22f5090a93356d8b4a3521e0810f6f72bc82
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-17 21:03:16 +0500

    Update Q7.c

--- Lab4/Q7.c
+++ Lab4/Q7.c
@@ -4,10 +4,10 @@ taken from the keyboard and display the total amount to pay to the customer.
 The charges are as follow:
 
         Unit                      Charge/Unit
-Up to 199                          @16.20
-200 and above but less than 300    @20.10
-300 and above but less than 500    @27.10
-500 and above @35.90
+Up to 199                          Rs.16.20
+200 and above but less than 300    Rs.20.10
+300 and above but less than 500    Rs.27.10
+500 and above                      Rs.35.90
 If the bill exceeds Rs. 18000 then a surcharge of 15% will be charged on top of the bill.
 
 Test Input:
@@ -19,7 +19,7 @@ Expected Output:
 Customer ID :1001
 Customer Name: James
 Units Consumed :800
-Amount Charges @Rs. 35.90 per unit: 28720
+Amount Charges Rs. 35.90 per unit: 28720
 Surcharge Amount: 4308
 Net Amount Paid by the Customer: 33028.00*/
 
@@ -27,34 +27,40 @@ Net Amount Paid by the Customer: 33028.00*/
 int main()
 {
 int customerID,units;
-float totalbill;
-char n;
-printf("Enter customer ID\n");
+float totalbill, Surcharge;
+char n[100];
+printf("Enter Customer ID: ");
 scanf("%d" , &customerID);
-printf("Enter customer name\n");
+printf("Enter Customer name: ");
 scanf("%s", &n);
-printf("enter units consumed\n");
+printf("Enter units consumed: ");
 scanf("%d", &units);
- if
-(units <= 199 && units>=0){
-totalbill=16.20*units;}
-  else if
-(units>=200 && units<300){
-totalbill=20.10*units;}
-    else if
-(units>=300 && units<500){
-totalbill=27.10*units;}
-      else
-{totalbill=35.90*units;}
- if
-(totalbill>18000){
-totalbill=totalbill+totalbill*0.15;}
+if(units >=0 && units<= 199)
+{
+    totalbill=16.20*units;
+}
+else if(units>=200 && units<300)
+{
+    totalbill=20.10*units;
+}
+else if(units>=300 && units<500)
+{
+    totalbill=27.10*units;
+}
+else
+{
+    totalbill=35.90*units;
+}
+if(totalbill>18000)
+{
+    Surcharge = totalbill*0.15;
+    totalbill=totalbill + Surcharge;
+}
 printf("\n Electric bill\n");
-printf("customer name; %s\n", n);
+printf("customer name: %s\n", n);
 printf("customer Id: %d\n", customerID);
 printf("units consumed: %d\n" , units);
+printf("Surcharge Amount: %.2f\n", Surcharge);
 printf("total bill: %.2f\n" , totalbill);
 return 0;
 }
-
-
