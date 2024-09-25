cb2e89c3218ae3d4ca99fe2d99dd681e44663fa0
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-25 22:20:58 +0500

    Create Q4.c

---
+++ Lab5/Q4.c
@@ -0,0 +1,4 @@
+/*Task4:
+Write a nested if statement for the decision diagrammed in the accompanying flowchart.
+ Use a multiple-alternative if statements for intermediate decisions where possible.
+*/
