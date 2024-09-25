2b3d59e43b9a9215308bfb617cb69f1b5dd39e1a
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-25 12:31:43 +0500

    Update Q1.c

--- Lab5/Q1.c
+++ Lab5/Q1.c
@@ -1,13 +1,21 @@
-/**/
+/*Task1: Write a program that asks for the number of calories and fat grams in a food. 
+ The program should display the percentage of calories that come from fat
+One gram of fat has 9 calories, so Calories from fat = fat grams * 9
+The percentage of calories from fat can be calculated as: calories from fat/total calories
+Input validation: Make sure the number of calories and fat grams are not less than 0.
+Also, the number of calories from fat cannot be greater than the total number of calories.
+If that happens, display an error message indicating that either the calories or fat grams were incorrectly entered.
+*/
+
 #include<stdio.h>
 int main()
 {
-	int  Fg , c , Cf;
+	float  Fg , c , Cf;
 	float Pc;
-	printf("enter number of calories ");
-	scanf("%d", &c);
+	printf("Enter number of calories ");
+	scanf("%f", &c);
 	printf("Enter number of Fat gram ");
-	scanf("%d", &Fg );
+	scanf("%f", &Fg );
 	if (c < 0 || Fg < 0)
 	{
 		printf("Invalid input");
@@ -20,6 +28,7 @@ int main()
 		printf("either the calories or the fat grams were incorrectly entered");
 		return 0 ; 
 	}
-	printf("Percentage of calories : %f " , Pc);
+	printf("Percentage of calories from Fat : %.2f " , Pc);
 
 }
+
