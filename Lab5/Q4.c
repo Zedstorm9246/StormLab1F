459ad16962a64bfa3b54b3796b2ac9c8b2d19ebb
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-25 22:36:17 +0500

    Update Q4.c

--- Lab5/Q4.c
+++ Lab5/Q4.c
@@ -2,3 +2,25 @@
 Write a nested if statement for the decision diagrammed in the accompanying flowchart.
  Use a multiple-alternative if statements for intermediate decisions where possible.
 */
+
+#include<stdio.h>
+int main()
+{
+    int PH;
+    printf("Enter the PH: ");
+    scanf("%d", &PH);
+    if(PH > 14 || PH < 0)
+    {
+        printf("PH out of bound");
+        return 0;
+    }
+    
+    if(PH > 7)
+    {
+        PH < 12 ? printf("Alkaline"): printf("Very Alkaline");
+    }
+    else 
+    {
+        PH == 7 ? printf("Neutral"): PH > 2 ? printf("Acidic"): printf("Very Acidic"); ;
+    }
+}
