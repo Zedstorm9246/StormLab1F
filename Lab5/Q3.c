0b71446d83585ed8f2f6124db16f22c6a2b92f28
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-25 14:13:13 +0500

    Create Q3.c

---
+++ Lab5/Q3.c
@@ -0,0 +1,2 @@
+/*Task3:
+Write a program to control a coffee machine. Allow the user to input the type of coffee as B for Black and W for White. Ask the user if the cup size is double and if the coffee is manual. The following table details the time chart for the machine for each coffee type. Display a statement for each step. If the coffee size is double, increase the baking time by 50 percent. Use functions to display instructions to the user and to compute the coffee time.*/
