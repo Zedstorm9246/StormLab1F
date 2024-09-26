f489a8329e151068f1270ed46795bc1e6299610c
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-09-26 13:40:02 +0500

    Update Q5.c

--- Lab5/Q5.c
+++ Lab5/Q5.c
@@ -1,3 +1,19 @@
 /*Task5:
 Implement the following decision table using nested ternary operators.
 Assume that the grade point average is within the range 0.0 through 4.0.*/
+
+
+#include<stdio.h>
+int main()
+{
+    float GPA;
+    printf("Enter GPA: ");
+    scanf(" %f", &GPA);
+    GPA < 0? printf("Invalid GPA"):
+    GPA < 1? printf("Failed semster-registration suspended"):
+    GPA < 2? printf("On probation for next semester"):
+    GPA < 3? printf(" "):
+    GPA < 3.5? printf("Dean's list for semester"):
+    GPA <= 4? printf("Highest honors for semester"):
+    printf("Invalid GPA");
+}
