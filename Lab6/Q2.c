3fe71e56b05a27d2ccf8192365091f42808ec3b4
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-10-03 15:04:29 +0500

    Update Q2.c

--- Lab6/Q2.c
+++ Lab6/Q2.c
@@ -4,12 +4,9 @@ int main()
 {
     int n;
     int is_prime = 0;
-
     printf("Enter n: ");
     scanf("%d", &n);
-
     int i = 2;
-
     while(i <= n)
     {
         if (n % i == 0 && i != 1 && i != n)
@@ -18,10 +15,8 @@ int main()
             break;
         }
         is_prime = 1;
-
         i++;
     }
-
     if (is_prime) printf("Prime");
     else printf("Not Prime");
     return 0;
