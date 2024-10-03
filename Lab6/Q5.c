d0c42ab233257d583f4ec70ec9e2a59fff561804
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2024-10-03 15:05:44 +0500

    Update Q5.c

--- Lab6/Q5.c
+++ Lab6/Q5.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
 int main()
 {
     for (int i = 0; i < 6; i++)
     {
         if (i == 0 || i == 5)
+        {
             printf("* * * * *\n");
+        }
         else
+        {
             printf("*       *\n");
+        }
     }
 }
